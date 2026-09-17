b9bdea738e2f2975383174e9c247a3e259d43598
Shine Shao <shine.shao@nagarro.com>
2026-09-17 20:20:47 +0800

    formatting

--- src/langgraphagenticai/state/state.py
+++ src/langgraphagenticai/state/state.py
@@ -1,4 +1,4 @@
-from typing_extensions import TypedDict,List
+from typing_extensions import TypedDict, List
 from langgraph.graph.message import add_messages
 from typing import Annotated
 
@@ -7,4 +7,5 @@ class State(TypedDict):
     """
     Represent the structure of the state used in graph
     """
-    messages: Annotated[List,add_messages]
+
+    messages: Annotated[List, add_messages]
--- src/langgraphagenticai/ui/streamlitui/loadui.py
+++ src/langgraphagenticai/ui/streamlitui/loadui.py
@@ -3,15 +3,17 @@ import os
 
 from src.langgraphagenticai.ui.uiconfigfile import Config
 
+
 class LoadStreamlitUI:
     def __init__(self):
-        self.config=Config()
-        self.user_controls={}
+        self.config = Config()
+        self.user_controls = {}
 
     def load_streamlit_ui(self):
-        st.set_page_config(page_title= "🤖 " + self.config.get_page_title(), layout="wide")
+        st.set_page_config(
+            page_title="🤖 " + self.config.get_page_title(), layout="wide"
+        )
         st.header("🤖 " + self.config.get_page_title())
-
 
         with st.sidebar:
             # Get options from config
@@ -21,16 +23,24 @@ class LoadStreamlitUI:
             # LLM selection
             self.user_controls["selected_llm"] = st.selectbox("Select LLM", llm_options)
 
-            if self.user_controls["selected_llm"] == 'Groq':
+            if self.user_controls["selected_llm"] == "Groq":
                 # Model selection
                 model_options = self.config.get_groq_model_options()
-                self.user_controls["selected_groq_model"] = st.selectbox("Select Model", model_options)
-                self.user_controls["GROQ_API_KEY"] = st.session_state["GROQ_API_KEY"] = st.text_input("API Key",type="password")
+                self.user_controls["selected_groq_model"] = st.selectbox(
+                    "Select Model", model_options
+                )
+                self.user_controls["GROQ_API_KEY"] = st.session_state[
+                    "GROQ_API_KEY"
+                ] = st.text_input("API Key", type="password")
                 # Validate API key
                 if not self.user_controls["GROQ_API_KEY"]:
-                    st.warning("⚠️ Please enter your GROQ API key to proceed. Don't have? refer : https://console.groq.com/keys ")
-            
+                    st.warning(
+                        "⚠️ Please enter your GROQ API key to proceed. Don't have? refer : https://console.groq.com/keys "
+                    )
+
             ## USecase selection
-            self.user_controls["selected_usecase"] = st.selectbox("Select Usecases",usecase_options)
+            self.user_controls["selected_usecase"] = st.selectbox(
+                "Select Usecases", usecase_options
+            )
 
         return self.user_controls
