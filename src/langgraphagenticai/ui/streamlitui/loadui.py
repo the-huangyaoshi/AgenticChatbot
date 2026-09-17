ac5cf8692a172e8c7b046ffae5fe3a0d894152f6
Shine Shao <shine.shao@nagarro.com>
2026-09-17 18:27:25 +0800

    feat: add Streamlit UI and configuration parser

--- src/langgraphagenticai/ui/streamlitui/loadui.py
+++ src/langgraphagenticai/ui/streamlitui/loadui.py
@@ -25,12 +25,12 @@ class LoadStreamlitUI:
                 # Model selection
                 model_options = self.config.get_groq_model_options()
                 self.user_controls["selected_groq_model"] = st.selectbox("Select Model", model_options)
-                self.user_controls["GROQ_API_KEY"] = st.session_state["GROQ_API_KEY"]=st.text_input("API Key",type="password")
+                self.user_controls["GROQ_API_KEY"] = st.session_state["GROQ_API_KEY"] = st.text_input("API Key",type="password")
                 # Validate API key
                 if not self.user_controls["GROQ_API_KEY"]:
                     st.warning("⚠️ Please enter your GROQ API key to proceed. Don't have? refer : https://console.groq.com/keys ")
             
             ## USecase selection
-            self.user_controls["selected_usecase"]=st.selectbox("Select Usecases",usecase_options)
+            self.user_controls["selected_usecase"] = st.selectbox("Select Usecases",usecase_options)
 
         return self.user_controls
--- src/langgraphagenticai/ui/uiconfigfile.py
+++ src/langgraphagenticai/ui/uiconfigfile.py
@@ -17,4 +17,3 @@ class Config:
     
     def get_page_title(self):
         return self.config["DEFAULT"].get("PAGE_TITLE")
-    
