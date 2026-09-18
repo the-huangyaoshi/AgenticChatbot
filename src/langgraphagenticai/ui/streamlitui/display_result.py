d17e86c861e7f45e6d94a0f0e43ffb6199975b85
the-huangyaoshi <shaoliang1214@163.com>
2026-09-18 13:29:53 +0800

    feat: add tool-integrated chatbot use case with web search, graph builder, and Streamlit UI components

--- src/langgraphagenticai/ui/streamlitui/display_result.py
+++ src/langgraphagenticai/ui/streamlitui/display_result.py
@@ -23,3 +23,21 @@ class DisplayResultStreamlit:
                         st.write(user_message)
                     with st.chat_message("assistant"):
                         st.write(value["messages"].content)
+
+        elif usecase == "Chatbot With Web":
+            # Prepare state and invoke the graph
+            initial_state = {"messages": [user_message]}
+            res = graph.invoke(initial_state)
+            print(f"Chatbot With Web Result : {res}")
+            for message in res["messages"]:
+                if type(message) == HumanMessage:
+                    with st.chat_message("user"):
+                        st.write(message.content)
+                elif type(message) == ToolMessage:
+                    with st.chat_message("ai"):
+                        st.write("Tool Call Start")
+                        st.write(message.content)
+                        st.write("Tool Call End")
+                elif type(message) == AIMessage and message.content:
+                    with st.chat_message("assistant"):
+                        st.write(message.content)
